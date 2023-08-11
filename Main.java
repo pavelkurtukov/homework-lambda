32fbdae6cc0e003cdd5918485db5205daa8739ec
Kurtukov Pavel <pavel@kurtukov.ru>
2023-08-11 20:56:39 +0700

    Worker added

--- Main.java
+++ Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
-
         int a = calc.plus.apply(1, 2);
-        int b = calc.minus.apply(1,1);
-        // Ошибка связана с делением на 0
+        int b = calc.minus.apply(1, 1);
+        // Ошибка, связанная с делением на 0
         //int c = calc.devide.apply(a, b);
         // Решение проблемы
         int c = calc.devide2.apply(a, b);
-
         calc.println.accept(c);
+
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
     }
 }
